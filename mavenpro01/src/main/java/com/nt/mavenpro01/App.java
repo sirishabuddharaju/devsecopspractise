203f78f958b2b90babfd0adbddb9b9c328fbf759
sirishabuddharaju <sirishabuddharaju2003@gmail.com>
2025-08-08 21:21:19 +0530

    'first push'

---
+++ mavenpro01/src/main/java/com/nt/mavenpro01/App.java
@@ -0,0 +1,14 @@
+package com.nt.mavenpro01;
+
+
+public class App 
+{
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+    public static void main( String[] args )
+    {
+      System.out.println("1234");
+      }
+}
